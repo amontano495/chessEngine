e9f8369147ea26c93ea5b231d1d7d28a1915a3fb
amontano495 <amontano@nevada.unr.edu>
2019-08-24 11:12:06 -0700

    added comments

--- chess.py
+++ chess.py
@@ -1,14 +1,21 @@
+#class definition for piece object
 class Piece:
 	def __init__(self, rank, player, position, possibleMoves):
+		#rank: King, Queen, Knight, etc
 		self.rank = rank
+		#White or Black
 		self.player = player
+		#tuple of position on board
 		self.position = position
+		#list of positions available to piece
 		self.possibleMoves = possibleMoves
 
 
 whiteSide = []
 blackSide = []
 
+#global board matrix
+#list of list of Piece objects
 board = []
 for i in range(8):
 	row = []
@@ -16,23 +23,27 @@ for i in range(8):
 		row.append(Piece("EMPTY","NONE",(i,j),[]))
 	board.append(row)
 
+#determines if tuple is outside of board limits
 def outOfBounds(position):
 	if (position[0] >= 0 and position[0] <= 7) and (position[1] >= 0 and position[1] <= 7):
 		return False
 	return True
 
+#returns opposite of given player string
 def enemySide(player):
 	if player == "WHITE":
 		return "BLACK"
 	else:
 		return "WHITE"
 
+#calculates possible moves for given piece
 def calcAllPositions(piece):
 	rank = piece.rank
 	position = piece.position
 
 	positionsList = []
 
+	#if piece is a knight, all "L" positions added to list
 	if rank == "KNIGHT":
 		positionsList.append((position[0] + 1, position[1] + 2))
 		positionsList.append((position[0] - 1, position[1] + 2))
@@ -44,22 +55,32 @@ def calcAllPositions(piece):
 		positionsList.append((position[0] + 2, position[1] - 1))
 		positionsList.append((position[0] - 2, position[1] - 1))
 
+	#if piece is a pawn
 	elif rank == "PAWN":
+
+		#if there is an enemy diagonally adjacent, can move there
 		for side,direction in zip(["WHITE","BLACK"],[-1,1]):
 			diagLeft = ( position[0] - 1, position[1] + direction )
 			diagRight = ( position[0] + 1, position[1] + direction)
 
+			#for each space diagonally up left, right
 			for square in [diagLeft,diagRight]:
+				#if space is not out of bounds
 				if not outOfBounds(square):
 					adjacentPiece = board[square[0]][square[1]]
 
+					#if space contains an enemy piece, add to list
 					if adjacentPiece.player == enemySide(piece.player):
 						positionsList.append(square)
 
+			#default movement is forward 1 and forward 2 spaces
 			positionsList.append((position[0] , position[1] + direction))
 			positionsList.append((position[0] , position[1] + direction*2))
 
+	#if piece is a queen
 	elif rank == "QUEEN":
+		#queen can move any direction, limitless
+
 		square = position
 		#up
 		while(not outOfBounds(square) ):
@@ -114,6 +135,7 @@ def calcAllPositions(piece):
 
 	invalidPosList = []
 
+	#remove positions with pieces on board of same side
 	for pos in positionsList:
 		if board[pos[0]][pos[1]].player == piece.player:
 			invalidPosList.append(pos)
@@ -121,21 +143,32 @@ def calcAllPositions(piece):
 	positionsList = list(set(positionsList) - set(invalidPosList))
 
 	return positionsList
-	
 
+#determines if move is acceptable by standard chess rules	
 def validMove(piece, target, player):
+	#check if piece being moved is owned by player
 	correctPlayer = piece.player == player
+
+	#calculate all possible moves for piece
 	possiblePositions = calcAllPositions(piece)
+
+	#if piece is owned by player and target is in move list
 	if correctPlayer and (target in possiblePositions):
 		return True
 	else:
 		return False
 
+#moves piece to target position on board
 def move(piece, target, player):
 	print(piece.possibleMoves)
 	if validMove(piece, target, player):
+		#replace previous position with empty
 		board[piece.position[0]][piece.position[1]] = Piece("EMPTY","NONE",piece.position,[])
+
+		#update piece position
 		piece.position = target
+
+		#update board position at target to piece
 		board[target[0]][target[1]] = piece
 		print(player + " MOVES " + piece.rank + " TO " + str(target))
 		return True
@@ -143,6 +176,7 @@ def move(piece, target, player):
 		print("INVALID MOVE")
 		return False
 
+#function to draw board contents to screen
 def drawBoard():
 	print('#'*24)
 	for i in range(8):
@@ -157,6 +191,7 @@ def drawBoard():
 		print('-'*27)
 	print("   |A |B |C |D |E |F |G |H ")
 
+#initializes the board, adds default pieces
 def initBoard():
 	#pawns
 	for i in range(8):
@@ -192,6 +227,7 @@ def initBoard():
 			blackSide.append(board[3][i])
 			blackSide.append(board[4][i])
 
+	#calculate possible moves for each piece on board
 	for i in range(8):
 		for j in range(8):
 				board[i][j].possibleMoves = calcAllPositions(board[i][j])
@@ -199,18 +235,21 @@ def initBoard():
 alphabet = "ABCDEFGH"
 coordDict = {alphabet[i] : i for i in list(range(8))}
 
+#converts algebraic notation to numeric tuples
 def anToMat(coords):
 	xcoord = int(coordDict[coords[0]])
 	ycoord = 8 - int(coords[1])
 
 	return (xcoord,ycoord)
 
+#gets king from side
 def getKing(side):
 	for i in range(8):
 		for j in range(8):
 			if board[i][j].rank == "kING" and board[i][j].player == side:
 				return board[i][j]
 
+#determines if a king is in check
 def determineCheck():
 	for side in ["WHITE","BLACK"]:
 		if side == "WHITE":
@@ -229,6 +268,7 @@ def determineCheck():
 				else:
 					return False
 
+#determines if game is in checkmate
 def determineCheckmate():
 	pieceVector = [piece for row in board for piece in row]
 	for side in ["WHITE","BLACK"]:
@@ -248,7 +288,6 @@ def determineCheckmate():
 	return False	
 		
 		
-
 def main():
 	initBoard()
 	currentPlayer = "WHITE"
